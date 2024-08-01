5c43576567fdf8e315d1eee97a5afec55adeb0ee
Wendoclement <wendoclement@gmail.com>
2024-08-01 10:29:56 +0300

    1- Setting up UI

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import HomePage from "./pages/home/HomePage";
+
+function App() {
+  return (
+    <div className="flex max-w-6xl mx-auto">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
